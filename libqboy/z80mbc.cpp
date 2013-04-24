033f960fa26fd3a6f349fe62b6c9bf2a8f8598e5
Mathijs van de Nes <mvdnes@mvdnes.nl>
2013-04-24 15:22:14 +0000

    MBC3 additions

--- libqboy/z80mbc.cpp
+++ libqboy/z80mbc.cpp
@@ -83,9 +83,21 @@ void z80mbc1::writeRAM(quint16 address, quint8 value) {
 /********************************/
 
 z80mbc3::z80mbc3(const std::vector<quint8> &rom) {
-	rtczero = time(NULL);
 	this->rom = rom;
-	ram.resize(0x10000);
+
+	int ramsize;
+	switch (rom[0x0149]) {
+	case 0x00:
+		ramsize = 0; break;
+	case 0x01:
+		ramsize = 0x800; break; // 2 kB
+	case 0x02:
+		ramsize = 0x2000; break; // 8 kB
+	case 0x03:
+		ramsize = 0x8000; break; // 32 kB
+	}
+
+	ram.resize(ramsize);
 	rtc.resize(5);
 	rombank = 1;
 	rambank = 0;
@@ -146,9 +158,19 @@ void z80mbc3::writeRAM(quint16 address, quint8 value) {
 
 void z80mbc3::save(std::string filename) {
 	std::ofstream fout(filename.c_str(), std::ios_base::out | std::ios_base::binary);
-	fout.write(reinterpret_cast<const char *>(&rtczero), sizeof(rtczero));
+
+	char head[] = "GB_MBC";
+	fout.write(head, sizeof head);
+	fout.write(reinterpret_cast<char *>(&rom[0x0147]), 1);
+
+	for (int i = 0; i < 5; ++i) {
+		fout.write(reinterpret_cast<char *>(&rtc[i]), 1);
+	}
+	quint64 rtc = rtczero;
+	fout.write(reinterpret_cast<char *>(&rtc), 8);
+
 	for (unsigned i = 0; i < ram.size(); ++i) {
-		fout.write((char*)&ram[i], 1);
+		fout.write(reinterpret_cast<char *>(&ram[i]), 1);
 	}
 	fout.close();
 }
@@ -157,14 +179,35 @@ void z80mbc3::load(std::string filename) {
 	std::ifstream fin(filename.c_str(), std::ios_base::in | std::ios_base::binary);
 	if (!fin.is_open()) return;
 
-	char byte;
-	ram.clear();
+	char head[7];
+	char ver;
 
-	fin.read(reinterpret_cast<char *>(&rtczero), sizeof(rtczero));
+	fin.read(head, 7);
+	fin.read(&ver, 1);
+	if (strcmp(head, "GB_MBC") != 0) {
+		fin.close();
+		return;
+	}
+
+
+	char byte;
+	rtc.clear();
+	for (int i = 0; i < 5; ++i) {
+		fin.read(&byte, 1);
+		rtc.push_back(byte);
+	}
+	quint64 rtc;
+	fin.read(reinterpret_cast<char *>(&rtc), 8);
+	rtczero = rtc;
+
+	ram.clear();
 	while (fin.read(&byte, 1)) {
 		ram.push_back(byte);
 	}
 	fin.close();
+
+
+	calc_rtcregs();
 }
 
 void z80mbc3::calc_rtczero() {
@@ -180,6 +223,10 @@ void z80mbc3::calc_rtczero() {
 }
 
 void z80mbc3::calc_rtcregs() {
+	if (rtc[4] & 0x40) {
+		return;
+	}
+
 	time_t difftime = time(NULL) - rtczero;
 	rtc[0] = difftime % 60;
 	rtc[1] = (difftime / 60) % 60;
@@ -187,6 +234,11 @@ void z80mbc3::calc_rtcregs() {
 	long long days = (difftime / (3600*24));
 	rtc[3] = days & 0xFF;
 	rtc[4] = (rtc[4] & 0xFE) | ((days >> 8) & 0x1);
+
+	if (days >= 512) {
+		rtc[4] |= 0x80;
+		calc_rtczero();
+	}
 }
 
 void z80mbc3::calc_halttime() {
